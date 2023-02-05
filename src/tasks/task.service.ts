13f487d1e6fccf8474be717b9ebed5597c31936b
Yazan Zoghbi <yazan.a.zoghbi@gmail.com>
2023-02-05 23:25:07 +0300

    [MOD] create task dto imported

--- src/tasks/task.service.ts
+++ src/tasks/task.service.ts
@@ -2,16 +2,18 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Task, TaskDocument } from './schemas/task.schema';
 import { Model } from 'mongoose';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 @Injectable()
 export class TaskService {
   private readonly tasks: Task[] = [];
   constructor(@InjectModel(Task.name) private taskModel: Model<TaskDocument>) {}
 
-  async create(task: Task) {
-    return this.taskModel.create(task);
+  async create(createTaskDto: CreateTaskDto) {
+    const createdTask = new this.taskModel(createTaskDto);
+    return createdTask.save();
   }
   async getAll(): Promise<Task[]> {
-    return await this.taskModel.find()
+    return await this.taskModel.find();
   }
 }
