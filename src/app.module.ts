96ca88a909a5effb33d289bae8cb33dfa5950276
Yazan Zoghbi <yazan.a.zoghbi@gmail.com>
2023-02-05 23:24:05 +0300

    [MOD] task module imported, mongodb server path fixed

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { TaskController } from './tasks/task.controller';
-import { TaskService } from './tasks/task.service';
+import { TaskModule } from './tasks/task.module';
 
 @Module({
-  imports: [MongooseModule.forRoot("mongodb://localhost/task-manager")],
-  controllers: [TaskController],
-  providers: [TaskService],
+  imports: [MongooseModule.forRoot("mongodb://localhost:27017/task-manager"), TaskModule],
+
 })
 export class AppModule {}
