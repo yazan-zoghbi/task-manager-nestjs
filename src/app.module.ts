52cac032fc4d138c8c0b18fe4e1a45329d967c2a
Yazan Zoghbi <yazan.a.zoghbi@gmail.com>
2023-02-05 22:19:31 +0300

    [MOD] name of imported files changed

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { AppController } from './app.controller';
-import { AppService } from './task.service';
+import { TaskController } from './tasks/task.controller';
+import { TaskService } from './tasks/task.service';
 
 @Module({
   imports: [MongooseModule.forRoot("mongodb://localhost/task-manager")],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [TaskController],
+  providers: [TaskService],
 })
 export class AppModule {}
