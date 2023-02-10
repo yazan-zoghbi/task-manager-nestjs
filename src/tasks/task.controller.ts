8199e898be5eec1f6f40893ba4d064ca6e72253d
Yazan Zoghbi <yazan.a.zoghbi@gmail.com>
2023-02-10 23:29:11 +0300

    [MOD] Param, Delete decorators imported

--- src/tasks/task.controller.ts
+++ src/tasks/task.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Get, Post, Put } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { TaskService } from './task.service';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
