2ab3300d843f7b4cffd9f4687b90516073e006c6
Yazan Zoghbi <yazan.a.zoghbi@gmail.com>
2023-02-11 14:00:34 +0300

    [MOD] update-status controller & service imported

--- src/tasks/task.module.ts
+++ src/tasks/task.module.ts
@@ -3,11 +3,13 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { TaskController } from "./task.controller";
 import { TaskService } from './task.service';
 import { Task, TaskSchema } from "./schemas/task.schema";
+import { UpdateStatusController } from "./update-status/update-status.controller";
+import { UpdateStatusService } from "./update-status/update-status.service";
 
 @Module({
     imports: [MongooseModule.forFeature([{name:Task.name , schema: TaskSchema}])],
-    controllers: [TaskController],
-    providers:[TaskService],
+    controllers: [TaskController, UpdateStatusController],
+    providers:[TaskService, UpdateStatusService],
 })
 
 export class TaskModule{}
