95f40572adec6cf4ce94ede81c15c140cb85655a
Yazan Zoghbi <yazan.a.zoghbi@gmail.com>
2023-02-17 14:30:20 +0300

    [add]: CRUD services added, thats include create, get one, update and delete user.

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,13 +1,35 @@
-import { Injectable } from "@nestjs/common";
-import { InjectModel } from "@nestjs/mongoose";
-import { Model } from "mongoose";
-import { User } from "./schema/user.schema";
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model, ObjectId } from 'mongoose';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './schema/user.schema';
 
 @Injectable()
-export class UserService{
-    constructor(@InjectModel(User.name) private userModel: Model<User>){}
+export class UserService {
+  constructor(
+    @InjectModel(User.name) private readonly userModel: Model<User>,
+  ) {}
 
-    async create(){
-        
-    }
+  async create(createUserDto: CreateUserDto) {
+    return await this.userModel.create(createUserDto);
+  }
+
+  async getOne(id: ObjectId) {
+    return await this.userModel.findById(id);
+  }
+
+  async update(id: ObjectId, updateUserDto: UpdateUserDto) {
+    return await this.userModel.updateOne({
+      _id: id,
+      name: {
+        firstName: updateUserDto.name.firstName,
+        lastName: updateUserDto.name.lastName,
+      },
+    });
+  }
+
+  async delete(id: ObjectId) {
+    return await this.userModel.deleteOne({ _id: id });
+  }
 }
