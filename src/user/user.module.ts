76fc3a2a2f12739e3f1f6382ec1e9f6c444ffa3d
Yazan Zoghbi <yazan.a.zoghbi@gmail.com>
2023-02-24 02:56:18 +0300

    [fix]: user schema issue with user services fixed after importing it

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { MongooseModule } from '@nestjs/mongoose';
+import { BcryptModule } from 'src/modules/bcrypt/bcrypt.module';
 import { User, UserSchema } from './schema/user.schema';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]), BcryptModule, JwtModule
   ],
   controllers: [UserController],
   providers: [UserService],
