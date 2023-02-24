5c0b1e10e881f5e8ecbd03306bd3c100bfb70b0b
Yazan Zoghbi <yazan.a.zoghbi@gmail.com>
2023-02-24 03:09:02 +0300

    [add]: class validator decorators added to class properties

--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,7 +1,12 @@
-export class UpdateUserDto {
-  name: {
-    firstName: String;
-    lastName: String;
-  };
+import { ApiProperty } from "@nestjs/swagger";
+import { IsString, isString } from "class-validator";
 
+export class UpdateUserDto {
+  @ApiProperty()
+  @IsString()
+  firstName: String;
+
+  @ApiProperty()
+  @IsString()
+  lastName: String;
 }
